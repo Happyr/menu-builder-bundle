d96a08d47d68ede5870390b29cb1baa87b90128d
Nyholm <tobias.nyholm@gmail.com>
2020-06-15 18:10:48 +0200

    Register services

--- src/DependencyInjection/HappyrMenuBuilderExtension.php
+++ src/DependencyInjection/HappyrMenuBuilderExtension.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace Happyr\MenuBuilderBundle\DependencyInjection;
 
+use Happyr\MenuBuilderBundle\MenuBuilder;
+use Happyr\MenuBuilderBundle\Twig\MenuExtension;
+use Happyr\MenuBuilderBundle\Twig\MenuRuntime;
 use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Reference;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class HappyrMenuBuilderExtension extends Extension
@@ -17,5 +21,16 @@ class HappyrMenuBuilderExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
+
+        $def = $container->register(MenuExtension::class);
+        $def->addTag('twig.extension');
+
+        $def = $container->register(MenuRuntime::class);
+        $def->setArguments([
+            new Reference(RequestStack::class),
+            new Reference(MenuBuilder::class),
+            $config['template']
+        ]);
+        $def->addTag('twig.runtime');
     }
 }
